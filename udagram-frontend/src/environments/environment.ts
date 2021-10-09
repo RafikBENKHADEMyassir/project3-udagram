89b52f111b3cbbe796562853fb0241b097dd5b7e
rafikbenkhadem <rafik.benkhadem@gmail.com>
2021-10-09 16:13:33 +0100

    adding deployment files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a320312224ec6438bbde664a9125f9a7-759930627.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
